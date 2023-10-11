bc2be5579531009ac97d76def86210e3c885728a
Sandesh <sandeshj@bsf.io>
2023-10-11 22:57:03 +0530

    Updated footer text

--- resources/js/Partials/Footer.jsx
+++ resources/js/Partials/Footer.jsx
@@ -13,8 +13,8 @@ export default function Footer() {
             <div className="relative border-t border-gray-900/10 sm:mt-16">
                 <div className="flex flex-col justify-center items-center md:flex-row md:text-left md:flex md:items-center md:justify-between py-4 md:py-8">
                     {/* Copyrights note */}
-                    <div className="mb-4 md:mb-0 text-sm text-gray-600">
-                        Tech stack{" "}
+                    <div className="mb-4 text-center md:text-left md:mb-0 text-sm text-gray-600">
+                        Made in ❤️ with Laravel & React | Tech -{" "}
                         <a
                             className="text-blue-600 hover:underline"
                             href="https://laravel.com"
@@ -83,23 +83,6 @@ export default function Footer() {
                                 </svg>
                             </a>
                         </li>
-
-                        {/* <li className="ml-4">
-                            <a
-                                href="#"
-                                target="_blank"
-                                className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
-                                aria-label="Facebook"
-                            >
-                                <svg
-                                    className="w-8 h-8 fill-current"
-                                    viewBox="0 0 32 32"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
-                                </svg>
-                            </a>
-                        </li> */}
                     </ul>
                 </div>
             </div>
